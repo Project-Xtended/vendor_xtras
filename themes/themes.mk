59c0813d091dba88a8bfc66cf907c93a06c313a4
SuperDroidBond <superdroidbond@yahoo.com>
2020-04-28 19:50:18 +0530

    VendorXtras: move themes & prebuilts to XtraSpace [2/2]

---
+++ themes/themes.mk
@@ -0,0 +1,14 @@
+# Themes
+PRODUCT_PACKAGES += \
+    PitchBlackSystemOverlay \
+    PitchBlackSystemUIOverlay \
+    SolarizedDarkSystemOverlay \
+    SolarizedDarkSystemUIOverlay \
+    BakedGreenSystemOverlay \
+    BakedGreenSystemUIOverlay \
+    ChocoXSystemOverlay \
+    ChocoXSystemUIOverlay \
+    MaterialOceanSystemOverlay \
+    MaterialOceanSystemUIOverlay \
+    DarkGreySystemOverlay \
+    DarkGreySystemUIOverlay
