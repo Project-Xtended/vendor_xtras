0ce9833b88852db97e0fdcbde643cec9360315fb
rajkale99 <kaleraj.rk@gmail.com>
2020-06-03 16:44:29 +0000

    Add GVM-URM-S overlay
    Change-Id: Ib836fdde02ca2cfbc3a67293d0652526d307503b

--- Gvm/gvm.mk
+++ Gvm/gvm.mk
@@ -1,15 +1,8 @@
 PRODUCT_PACKAGES += \
-	GVM-PG-IOS \
-        GVM-PG-OOS \
-        GVM-PG-WDE \
         GVM-SBH-L \
         GVM-SBH-M \
         GVM-SBH-XL \
         GVM-URM-M \
         GVM-URM-L \
         GVM-URM-R \
-        GVM-URM-V-L \
-        GVM-URM-V-M \
-        GVM-URM-V-R
-
-
+        GVM-URM-S
