890870901951a30857bd17ce3b2a07be0810e80e
SuperDroidBond <superdroidbond@yahoo.com>
2021-01-02 21:02:33 +0530

    Switch: update for RRO packages

--- Switch/Stock/Android.mk
+++ Switch/Stock/Android.mk
@@ -1,10 +1,9 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-CAL_MODULE_TAGS := optional
-
+LOCAL_RRO_THEME := SwitchStock
+LOCAL_PRODUCT_MODULE := true
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_PACKAGE_NAME := SwitchStock
 LOCAL_SDK_VERSION := current
-LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := false
-
-include $(BUILD_PACKAGE)
+include $(BUILD_RRO_PACKAGE)
