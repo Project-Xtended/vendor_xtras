77df06edba4e39c09bda8bd47ad7c3e871065c31
Maitreya29 <maitreyapatni30@gmail.com>
2021-02-25 18:00:39 +0000

    xtras: QS Header size customizations. [3/3]
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- qsthemes/qsthemes.mk
+++ qsthemes/qsthemes.mk
@@ -12,6 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# QS header size
+PRODUCT_PACKAGES += \
+    HeaderLarge \
+    HeaderXLarge
+
 # QS tile styles
 PRODUCT_PACKAGES += \
     QStileCircleTrim \
