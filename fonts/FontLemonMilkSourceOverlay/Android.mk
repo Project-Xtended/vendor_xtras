84a3ff9afe38999b0f05dd150c7c062b9dbbba19
Sonal Singh <sonal.singh.19993@gmail.com>
2021-04-28 15:11:54 +0530

    xtras: Move all font overlays to vendor/xtras [2/2]
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- fonts/FontLemonMilkSourceOverlay/Android.mk
+++ fonts/FontLemonMilkSourceOverlay/Android.mk
@@ -1,26 +1,31 @@
-# Copyright (C) 2020-21 The Project-Xtended
+#
+#  Copyright 2020, Xtended Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
 
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+LOCAL_RRO_THEME := FontLemonMilkSourceOverlay
+LOCAL_CERTIFICATE := platform
+LOCAL_PRODUCT_MODULE := true
 
-# Copy fonts to system
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xtras/fonts,$(TARGET_COPY_OUT_SYSTEM)/fonts)
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
-# Includes fonts
-include vendor/xtras/fonts/fonts.mk
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
-# Includes prebuilts
-include vendor/xtras/prebuilts/prebuilts.mk
+LOCAL_PACKAGE_NAME := FontLemonMilkSourceOverlay
+LOCAL_SDK_VERSION := current
+
+include $(BUILD_RRO_PACKAGE)
