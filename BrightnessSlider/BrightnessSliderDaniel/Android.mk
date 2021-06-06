375047a9e3593c88961b957f394d11566da2a0e9
SuperDroidBond <superdroidbond@yahoo.com>
2021-06-07 00:56:37 +0530

    BrightnessSliderStyles: update for RRO packages

--- BrightnessSlider/BrightnessSliderDaniel/Android.mk
+++ BrightnessSlider/BrightnessSliderDaniel/Android.mk
@@ -15,11 +15,13 @@
 #
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-CAL_MODULE_TAGS := optional
 
+LOCAL_RRO_THEME := BrightnessSliderDaniel
 LOCAL_PACKAGE_NAME := BrightnessSliderDaniel
+LOCAL_PRODUCT_MODULE := true
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_SDK_VERSION := current
-LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := false
 
-include $(BUILD_PACKAGE)
+include $(BUILD_RRO_PACKAGE)
+
