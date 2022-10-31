dd92421791e44fa84eeebabb2fbd2843b73ded86
mukesh22584 <mks22584@gmail.com>
2022-10-31 23:25:35 +0530

    xtras: import fonts

--- fonts/FontComicSansSourceOverlay/Android.mk
+++ fonts/FontComicSansSourceOverlay/Android.mk
@@ -1,22 +1,31 @@
-# Copyright (C) 2022 Project-Xtended
+#
+#  Copyright 2019, The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-LOCAL_PATH := $(call my-dir)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
 
-# Copy fonts to system
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xtras/fonts,$(TARGET_COPY_OUT_SYSTEM)/fonts)
+LOCAL_RRO_THEME := FontComicSansSourceOverlay
+LOCAL_CERTIFICATE := platform
+LOCAL_PRODUCT_MODULE := true
 
-# Includes fonts
-include vendor/xtras/fonts/fonts.mk
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+
+LOCAL_PACKAGE_NAME := FontComicSansSourceOverlay
+LOCAL_SDK_VERSION := current
+
+include $(BUILD_RRO_PACKAGE)
