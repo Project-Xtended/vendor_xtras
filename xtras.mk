cdd7ee07c0f26f777f2725ad8ede00974e7c2ba7
rdx420 <padraramesh420@gmail.com>
2023-02-08 08:02:39 +0800

    qs themes: Add 6 new QS styles
     *Thanks to Iconify
    
    Signed-off-by: rdx420 <padraramesh420@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- xtras.mk
+++ xtras.mk
@@ -145,7 +145,13 @@ PRODUCT_PACKAGES += \
 # QS Panel
 PRODUCT_PACKAGES += \
     QSOutline \
-    QSTwoToneAccent
+    QSTwoToneAccent \
+    QSShaded \
+    QSCyberPunk \
+    QSNeumorph \
+    QSReflected \
+    QSSurround \
+    QSThin
     
 # Statusbar Icons
 PRODUCT_PACKAGES += \
